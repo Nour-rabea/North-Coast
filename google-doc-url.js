f01742a2127af5a235c4557c18775917cfe4a46b
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2020-12-17 17:30:28 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1eaOruwwMIgsK_exX37SDh4X7KwMGUvIlbaUnatwEseA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBRBVqGWH7Ibcr30SpLU-J49N9BCg_6uqE';
+var googleApiKey = '';
