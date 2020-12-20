46a41cfd9d4e3042e7dd3c872277643391c4341a
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2020-12-20 16:11:23 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -5,7 +5,7 @@ $(window).on('load', function() {
   var polygonSettings = [];
   var polygonsLegend;
 
-  var completePoints = true;
+  var completePoints = false;
   var completePolygons = false;
   var completePolylines = false;
 
