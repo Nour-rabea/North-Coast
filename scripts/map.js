bfbe866cf9d58cc35c30c6a1ff971678892248d7
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2020-12-23 10:47:45 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -127,7 +127,7 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Developer']);
+          point['Description']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
