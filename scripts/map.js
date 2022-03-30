35e4d3e812d4faf158cb533cfb4b739695be99b2
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2022-03-30 18:45:49 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -265,14 +265,6 @@ $(window).on('load', function() {
     completePoints = true;
     return group;
   }
-  
-  var table = getseting(('_points'));
-  tablerows.onclick = function zoom() {
-    var lat = map.getCenter().lat, latSet = false;
-    var lon = map.getCenter().lng, lonSet = false;
-    var zoom = 12, zoomSet = false;
-    var center;
-  }
 
 
   var polygon = 0; // current active polygon
